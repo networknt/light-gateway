8fd658c951e00f25f710cd08178fa3b1d76c087a
Steve Hu <stevehu@gmail.com>
2023-10-26 14:21:46 -0400

    fixes #151 add jdk 17 docker file and update build.sh (#152)

--- build.sh
+++ build.sh
@@ -44,6 +44,7 @@ publish() {
     echo "Building Docker image with version $VERSION"
     docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest -f ./docker/Dockerfile . --no-cache=true
     docker build -t $IMAGE_NAME:$VERSION-slim -f ./docker/Dockerfile-Slim . --no-cache=true
+    dokcer build -t $IMAGE_NAME:$VERSION-17 -f ./docker/Dockerfile.17 . --no-cache=true
     echo "Images built with version $VERSION"
     echo "Pushing image to DockerHub"
     docker push $IMAGE_NAME -a
