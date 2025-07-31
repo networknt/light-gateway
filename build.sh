d2c517d5216a0dbbce0c1985807903ddec96f352
Steve Hu <stevehu@gmail.com>
2025-07-30 21:12:07 -0400

    fixes #224 remove the dockerfile that is based on java 17

--- build.sh
+++ build.sh
@@ -39,7 +39,6 @@ publish() {
     echo "Building Docker image with version $VERSION"
     docker build -t $IMAGE_NAME:$VERSION -t $IMAGE_NAME:latest -f ./docker/Dockerfile . --no-cache=true
     docker build -t $IMAGE_NAME:$VERSION-slim -f ./docker/Dockerfile-Slim . --no-cache=true
-    docker build -t $IMAGE_NAME:$VERSION-17 -f ./docker/Dockerfile.17 . --no-cache=true
     echo "Images built with version $VERSION"
 
     if $LOCAL_BUILD; then
